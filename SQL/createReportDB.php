4f0a2dd0422e892a02c94d8c257ff355f763406e
Marcus A <marcus.abraham100@gmail.com>
2023-03-09 15:25:32 +0000

    Changing map markers, and removing alternate address

--- SQL/createReportDB.php
+++ SQL/createReportDB.php
@@ -8,7 +8,6 @@ if($_POST['phpFunction'] == 'create')
 
 		$userID = 1;
         $issueType = $_POST['issueSelect'];
-        $alternateAddress = $_POST['manualAddress'];
         $long = $_POST['lng'];
         $lat = $_POST['lat'];
         $problemDescription = $_POST['problemDescription'];
@@ -20,14 +19,9 @@ if($_POST['phpFunction'] == 'create')
         //NEED TO ADD
         //Fet current user ID (once log in is working)
         //Get lat/long from the alternate address field
-        if (is_null($alternateAddress)){
-            $sql = "INSERT INTO `report_table`(user_id, type, long, lat, description, report_status, date_reported)"."values".
-			   "($userID, '$issueType', '$long', '$lat', '$problemDescription', '$reportStatus', '$dateReported')";
-        }
-        else{
-            $sql = "INSERT INTO `report_table`(user_id, type, description, report_status, date_reported)"."values".
-			   "($userID, '$issueType', '$problemDescription', '$reportStatus', '$dateReported')";
-        }
+        $sql = "INSERT INTO `report_table`(user_id, type, long, lat, description, report_status, date_reported)"."values".
+			"($userID, '$issueType', '$long', '$lat', '$problemDescription', '$reportStatus', '$dateReported')";
+
         //Create an insert for the images table, linking the report_id with each image submitted
 
         //$sql = "INSERT INTO `images_table`()"."values".
