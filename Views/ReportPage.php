ea862eb05226e7f1dc34190598469d7495e55fa7
bensolo12 <bensolomonsflute@gmail.com>
2023-03-10 01:04:42 +0000

    Update ReportPage.php

--- Views/ReportPage.php
+++ Views/ReportPage.php
@@ -329,6 +329,10 @@
         lngInput.value = newLng;
         }
       );
+
+      google.maps.event.addListener(marker, 'dragend', function(event){
+        updateMarkerPosition(this.position, true, true);
+      });
     </script>
 
     <script>
