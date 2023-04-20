52af932c81ff4f080df122dd0bece79b6b32ab5c
Mathew-Bushell <98354803+Mathew-Bushell@users.noreply.github.com>
2023-04-20 14:13:09 +0100

    cock

--- PHP/dbConfig.php
+++ PHP/dbConfig.php
@@ -1,9 +1,9 @@
 <?php
 // connection to the sql database
-  $servername = "localhost";
-  $username = "CT4034BTUser";
-  $password = "E6@g9xx9";
-  $dbname = "s4005098_CT4034DB";
+    $servername = "localhost";
+    $username = "testUser";
+    $password = "J1269c@qp";
+    $dbname = "TTCMM";
 
   $connection = new mysqli($servername, $username, $password, $dbname);
 
