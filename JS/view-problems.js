fe2eb926a5554ebffa006a604d48bec74f806c10
bensolo12 <bensolomonsflute@gmail.com>
2023-04-21 13:56:03 +0100

    Update view-problems.js

--- JS/view-problems.js
+++ JS/view-problems.js
@@ -72,8 +72,8 @@ function displayComments(reportId){
                 document.getElementById("commentDate").textContent = "Comment date: " + commentDate;
                 document.getElementById("commentText").textContent = "Comment text: " + commentText;
                 document.getElementById("addcomment").textContent = "Add Comment";
-                document.getElementById("addcomment").remove("hidden");
-                document.getElementById("addCommentsField").remove("hidden");
+                document.getElementById("addcomment").removeAttribute("hidden");
+                document.getElementById("addCommentsField").removeAttribute("hidden");
                 document.getElementById("addcomment").addEventListener("click", createComment);
                 
                 
