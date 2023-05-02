cb9090885b387b2e30356c969d5e77d091f2b6c6
bensolo12 <bensolomonsflute@gmail.com>
2023-05-02 03:45:15 +0100

    Update ReportPage.php

--- Views/ReportPage.php
+++ Views/ReportPage.php
@@ -165,6 +165,8 @@
         <li><a href="Index.html">Home</a></li>
         <li><a class="NavActive" href="ReportPage.php">Report Issue</a></li>
         <li><a href="ContactUs.html">Contact Us</a></li>
+        <li><a href="view-problems.html">View Problems</a></li>
+        <li id="NavSignIn" style="float:right"><a class="NavActive" href="login.html" id="NavSignIn">Sign In</a></li>
         <li style="float:right"><a href="javascript:logout()" id="NavSignOut">Sign Out</a></li>
         <li style="float:right;display: flex; justify-content: flex-end;"><input type="text" name="Search" value=""
             placeholder="Search"></li>
@@ -368,6 +370,7 @@
   } catch (Exception $e) {
   }
   if ($userID != null):
+    echo "<script src='../JS/Common.js'></script>";
     echo "<script src='../JS/submitReport.js'></script>";
     echo "<script src='../JS/reportFormHide.js'></script>";
     echo "<script src='../JS/reportFormOther.js'></script>";
