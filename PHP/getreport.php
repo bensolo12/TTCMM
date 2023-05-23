0fdc21db696c84837338313d8a7bbe5e12244cfa
bensolo12 <bensolomonsflute@gmail.com>
2023-05-23 15:46:20 +0100

    Update getreport.php

--- PHP/getreport.php
+++ PHP/getreport.php
@@ -26,6 +26,7 @@
     $row = mysqli_fetch_assoc($res);
     
     $role = $_SESSION['user_role'];
+    console.log("role is"+$role);
     //If there's one row in the result then encode the row and echo it back
     if($num_row == 1){
         echo json_encode($row);
